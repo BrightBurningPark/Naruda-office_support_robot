f8d909baa380f7d9b87ffa5000cf1da106948b71
KimDHyun <45141385+KimDHyun@users.noreply.github.com>
2019-05-02 20:51:28 +0900

    update package

--- web-app/src/components/App.js
+++ web-app/src/components/App.js
@@ -2,8 +2,17 @@
 
 import React, { Component } from 'react'
 import './App.css'
+import './CenterArea.js'
+import './LeftBar.js'
+import './TeskList.js'
+
+
 
 class App extends Component {
+  
+  
+  
+  
   render() {
     return (
       <div>
