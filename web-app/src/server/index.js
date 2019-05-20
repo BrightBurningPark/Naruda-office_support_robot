49a8ddbdc9a21d498be8a3fdf604506e15601344
crostreet74 <crosstreet74@gmail.com>
2019-05-20 23:25:07 +0900

    clean dependency

--- web-app/src/server/index.js
+++ web-app/src/server/index.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const socketIO = require('socket.io')
 const app = express()
+const cors = require('cors')
 // const mysql = require('mysql')
 // const dbconfig   = require('./config/database.js')
 // const connection = mysql.createConnection(dbconfig)
@@ -11,6 +12,8 @@ const PORT = process.env.PORT || 3000
 process.on('uncaughtException', function (err) {
   console.log(err);
 });
+
+app.use(cors());
   
 app.use('/', express.static(__dirname + '/../../dist'));
 
