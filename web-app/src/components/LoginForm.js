b9bbb4cd0ddeb579d9e369660cf07cc78d578d12
crostreet74 <crosstreet74@gmail.com>
2019-05-23 02:39:21 +0900

    server handling id,password req

--- web-app/src/components/LoginForm.js
+++ web-app/src/components/LoginForm.js
@@ -1,28 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react'
 import 'semantic-ui-less/semantic.less';
 import { Divider, Button, Form, Grid, Header, Segment } from 'semantic-ui-react'
+import axios from 'axios'
 
-const LoginForm = () => (
-    <div className='login-form'>
-        <style>
-            {`body > div,body > div > div,body > div > div > div.login-form {height: 100%;}`}
-        </style>
-        <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
-            <Grid.Column style={{ maxWidth: 450 }}>
-                <Header as='h2' color='teal' textAlign='center'>Naruda</Header>
-                <Form size='large'>
-                    <Segment stacked>
-                        <Form.Input fluid icon='user' iconPosition='left' placeholder='Email address' />
-                        <Form.Input fluid icon='lock' iconPosition='left' placeholder='Password' type='password' />
-                        <Button color='teal' fluid size='large' to="/Main">
-                        </Button>
-                    </Segment>
-                </Form>
-                <Divider horizontal>Or</Divider>
-                <Button color='teal' fluid size='large'>Sign Up</Button>
-            </Grid.Column>
-        </Grid>
-    </div>
-)
 
-export default LoginForm
+class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { email: '', password: '' }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
+    handleSubmit = () => {
+        axios.post('http://localhost:3000/signin', {
+            email: 'admin',
+            password: 'admin'
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
+
+    render() {
+        const { email, password } = this.state
+
+        return (
+            <div>
+                <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+                    <Grid.Column style={{ maxWidth: 450 }}>
+                        <Header as='h2' color='teal' textAlign='center'>Naruda</Header>
+                        <Form size='large' onSubmit={this.handleSubmit}>
+                            <Segment stacked>
+                                <Form.Input fluid icon='user' iconPosition='left' placeholder='Email Address' name='email' value={email} onChange={this.handleChange} />
+                                <Form.Input fluid icon='lock' iconPosition='left' placeholder='Password' type='password' name='password' value={password} onChange={this.handleChange} />
+                                <Form.Button color='teal' fluid size='large' content='Submit' />
+                            </Segment>
+                        </Form>
+                        <Divider horizontal>Or</Divider>
+                        <Button color='teal' fluid size='large'>Sign Up</Button>
+                    </Grid.Column>
+                </Grid>
+
+                {/* to be removed */}
+                <strong>onChange:</strong>
+                <pre>{JSON.stringify({ email, password }, null, 2)}</pre>
+            </div>
+        )
+    }
+}
+
+export default LoginForm;
--- web-app/src/server/index.js
+++ web-app/src/server/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const socketIO = require('socket.io')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
 // const mysql = require('mysql')
 // const dbconfig   = require('./config/database.js')
 // const connection = mysql.createConnection(dbconfig)
@@ -14,8 +15,26 @@ process.on('uncaughtException', function (err) {
 });
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
   
 app.use('/', express.static(__dirname + '/../../dist'));
+
+app.post('/signin', function (req, res) {
+  var email=req.body.email;
+  var password=req.body.password;
+  if(email=='admin' && password=='admin'){
+  	res.send('success');
+  }
+  else{
+  	res.send('Failure');
+  }
+})
 
 const server = app.listen(PORT, (err) => {
   if (err) {
