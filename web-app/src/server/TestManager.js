e2249c35c6ff495a569864c6f793164326c9ed23
crostreet74 <crosstreet74@gmail.com>
2019-06-10 01:21:58 +0900

    able to run test TaskManager

--- web-app/src/server/TestManager.js
+++ web-app/src/server/TestManager.js
@@ -1,13 +1,13 @@
-exports = module.exports = function (io_web, io_narumi) {
+exports = module.exports = function (io) {
 
     io.on('connection', socket => {
         console.log('client connected: ', socket.client.id)
 
         socket.on('new_task', (message) => {
             console.log('message : ' + message)
-            new_task = [message.toXcoord, message.toYcoord, 'B']
+            var new_task = [message.toXcoord, message.toYcoord, 'B']
             console.log('new_task : ' + new_task)
-            socket.emit('start_move', new_task)
+            socket.broadcast.emit('start_move', new_task)
         })
 
         socket.on('disconnect', () => {
