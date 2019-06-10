2b873c178d42bf6cdee60eefa70915bd7750f9be
crostreet74 <crosstreet74@gmail.com>
2019-06-11 01:57:00 +0900

    clean tested resources

--- web-app/src/client/components/Header.js
+++ web-app/src/client/components/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react'
-import { Container, Button, Menu, Visibility } from 'semantic-ui-react'
+import { Container, Button, Menu } from 'semantic-ui-react'
 
 /*
 const menuStyle = {
